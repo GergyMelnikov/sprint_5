0e9d2f179afa7b7cd5ec2d1f86b7c99b8aa3996c
gmel <gmel003@icloud.com>
2024-04-16 00:30:51 +0300

    Разобрался как импортировать модули с разных уровней вложенности

--- tests/registration_success.py
+++ tests/registration_success.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from generators.email_and_password_generators import generate_email
+from generators.email_and_password_generators import generate_password
+# без таких манипуляций VS Code не ест импорт из каталогов на разных уровнях
+
 import time
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -6,12 +15,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common import by
 
 
+
+
+
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 10)
 driver.get('https://stellarburgers.nomoreparties.site/')
 
+print(generate_email())
+print(generate_password(6))
 
 
-
-
+time.sleep(10)
 driver.quit()
