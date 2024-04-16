e375829206c27dfbe8e8af683c3359afbc0a54cc
gmel <gmel003@icloud.com>
2024-04-16 22:14:58 +0300

    Добавил тестирование выхода, перехода в личный кабинет и перехода в конструктор

---
+++ tests/account_profile.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
+driver.get('https://stellarburgers.nomoreparties.site/login')
+
+email = 'Account_for_sign_in_testing@yaya.ru'
+password = '123123'
+
+driver.find_element(By.XPATH, '/html/body/div/div/main/div/form/fieldset[1]/div/div/input').send_keys(email)
+driver.find_element(By.CSS_SELECTOR, 'input[type="password"]').send_keys(password)
+driver.find_element(By.CSS_SELECTOR, 'button[class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]').click()
+
+wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/header/nav/a/p'))).click()
+wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/main/div/div/div/ul/li[2]/div/div/input')))
+
+assert driver.current_url == 'https://stellarburgers.nomoreparties.site/account/profile'
+
+driver.quit()
