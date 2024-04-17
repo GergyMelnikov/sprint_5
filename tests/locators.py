c293501325fd2d075435ae180a61797d3bce4ed7
georgiy <georgiy.melnikov@webasyst.com>
2024-04-17 15:36:36 +0300

    Добавил недостающие описания локаторам

--- tests/locators.py
+++ tests/locators.py
@@ -21,10 +21,10 @@ class Forgot_password_form_locators():
     SIGN_IN_LINK = (By.CSS_SELECTOR, 'a[href="/login"]') # Ссыкла "Войти" в форме восстановления пароля и в форме регистрации
 
 class Main_page_locators():
-    SIGN_IN_BUTTON = (By.XPATH, '/html/body/div/div/main/section[2]/div/button')
+    SIGN_IN_BUTTON = (By.XPATH, '/html/body/div/div/main/section[2]/div/button') # Кнопка входа на главной
 
 class Registration_form_locators():
-    PASSWORD_FIELD = (By.CSS_SELECTOR, 'input[type="password"]')
-    SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button[class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]')
-    NAME_FIELD = (By.XPATH, '/html/body/div/div/main/div/form/fieldset[1]/div/div/input')
-    EMAIL_FIELD = (By.XPATH, '/html/body/div/div/main/div/form/fieldset[2]/div/div/input')
+    PASSWORD_FIELD = (By.CSS_SELECTOR, 'input[type="password"]') # Поле для ввода пароял в форме регистрации
+    SUBMIT_BUTTON = (By.CSS_SELECTOR, 'button[class="button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa"]') # Кнопка сабмита в форме регистрации
+    NAME_FIELD = (By.XPATH, '/html/body/div/div/main/div/form/fieldset[1]/div/div/input') # Поле для ввода имени в форме регистрации
+    EMAIL_FIELD = (By.XPATH, '/html/body/div/div/main/div/form/fieldset[2]/div/div/input') # Поле для адреса электронной почты, в форме регистрации
